c00487cb39b3f7340d052e36afb3ad3408bd7e95
Hai Nam <nam9x027@gmail.com>
2023-09-17 22:19:31 +0700

    sửa giao diện đăng nhập

--- app/Http/Controllers/Admin/Users/LoginController.php
+++ app/Http/Controllers/Admin/Users/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin\Users;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -12,5 +13,20 @@ class LoginController extends Controller
         return view('admin.users.login', [
             'title' => 'Đăng nhập hệ thống'
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email:filter',
+            'password' => 'required'
+        ]);
+
+        if(Auth::attempt(['email' => $request->input('email'), 
+                        'password' => $request->input('password')
+        ], $request -> input('remember'))) {
+            return redirect()->route('admin');
+        }
+        return redirect() -> back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\Users\LoginController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('admin/users/login', [LoginController::class, 'index']);
+Route::post('admin/users/login/store', [LoginController::class, 'store']);
+
+Route::get('admin/main', [MainController::class, 'index'])->name('admin');
 
