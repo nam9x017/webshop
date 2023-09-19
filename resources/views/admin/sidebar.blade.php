cd4098767be6712b10ca4b80d2dccdc6102ada6f
Hai Nam <nam9x027@gmail.com>
2023-09-19 09:56:17 +0700

    Menu tạo bảng

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -48,21 +48,15 @@
                            </a>
                            <ul class="nav nav-treeview">
                                <li class="nav-item">
-                                   <a href="../../index.html" class="nav-link">
+                                   <a href="/admin/menus/add" class="nav-link">
                                        <i class="far fa-circle nav-icon"></i>
-                                       <p>Dashboard v1</p>
+                                       <p>Thêm Danh Mục</p>
                                    </a>
                                </li>
                                <li class="nav-item">
-                                   <a href="../../index2.html" class="nav-link">
+                                   <a href="/admin/menus/list" class="nav-link">
                                        <i class="far fa-circle nav-icon"></i>
-                                       <p>Dashboard v2</p>
-                                   </a>
-                               </li>
-                               <li class="nav-item">
-                                   <a href="../../index3.html" class="nav-link">
-                                       <i class="far fa-circle nav-icon"></i>
-                                       <p>Dashboard v3</p>
+                                       <p>Danh Sách Danh Mục</p>
                                    </a>
                                </li>
                            </ul>
